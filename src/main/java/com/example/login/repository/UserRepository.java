afaec61fbe4e828ebf128ca8d34de0ce276ccbbb
ravimrinal <mrinal.ravi@ibsplc.com>
2021-12-09 11:33:28 +0530

    initial commit

---
+++ src/main/java/com/example/login/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.login.repository;
+
+import com.example.login.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+
+}
